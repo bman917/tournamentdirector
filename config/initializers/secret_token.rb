90b584eae9abefb1d271341775859d7b00193209
jchan <bman917@gmail.com>
2013-06-25 00:50:50 +0800

    initial newly created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tournamentdirector::Application.config.secret_key_base = '95159fcd79b13fd45849ff90a203882660808041cdfa37f54bfa3e54a0e1fa459419b4ca6fb63508c6576e95a1bc95a4b9845c26c5af5b0b0ac9e6a75d029e2c'
