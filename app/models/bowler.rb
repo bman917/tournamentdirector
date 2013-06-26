f832277523737e7f192df190b045e6231da2bbcd
jchan <bman917@gmail.com>
2013-06-26 23:07:34 +0800

    enforce uniqueness of bowlers and association names

--- app/models/bowler.rb
+++ app/models/bowler.rb
@@ -1,4 +1,5 @@
 class Bowler < ActiveRecord::Base
   validates :name, presence: true
+  validates_uniqueness_of :name
   belongs_to :bowling_association
 end
--- app/models/bowling_association.rb
+++ app/models/bowling_association.rb
@@ -1,4 +1,5 @@
 class BowlingAssociation < ActiveRecord::Base
 	has_many :bowlers
 	validates :name, presence: true
+	validates_uniqueness_of :name
 end
