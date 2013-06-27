9eae37be506cc05a8c624be2ee001e2e4a2b2d8b
jchan <bman917@gmail.com>
2013-06-28 01:28:00 +0800

    enforce login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_action :require_login
+ 
+  private
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
-	private
+  def logged_in?
+  	session[:user_id]
+  end
 
-	def current_user
-	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
-	end
-
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to log_in_path # halts request cycle
+    end
+  end
 
 end
