38933f683d00349e3b3a8b00dae4347d0dac5306
jchan <bman917@gmail.com>
2013-07-01 02:15:57 +0800

    fix bug with missing tournament id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def selected_tournament
-    @selected_tournament ||= Tournament.find(session[:selected_tournament]) if session[:selected_tournament]
+    @selected_tournament ||= Tournament.find_by_id(session[:selected_tournament]) if session[:selected_tournament]
   end
 
   def logged_in?
