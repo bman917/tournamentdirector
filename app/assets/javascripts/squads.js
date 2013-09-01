256b20c7f7b5fbcfbb44d9ab1a0f1e6e6706509e
jchan <bman917@gmail.com>
2013-09-01 23:53:09 +0800

    squad show UI overhaul

--- app/assets/javascripts/squads.js
+++ app/assets/javascripts/squads.js
@@ -3,9 +3,13 @@ $(document).on('change', '#tournament_game_type_name', filterTable);
 
 function filterTable() {
 	var category_css = $('#tournament_bowlers_class_name').val();
+	var category_css_text = $('#tournament_bowlers_class_name option:selected').text();
 	var game_type = $('#tournament_game_type_name').val();
 	$('tr.data').hide();
 	$('tr.' + category_css + '.' + game_type).show();
+
+	$('#bowler_category_header').text('Bowlers - ' + game_type + ' ' + category_css_text + ' Division');
+	
 }
 
 function showAddingRow() {
