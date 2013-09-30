246484a277ca07fed01103449c595dc0d2b215b3
jchan <bman917@gmail.com>
2013-09-30 22:31:24 +0800

    switch to mysqldb

--- db/migrate/20130916102548_create_install_script_user.rb
+++ db/migrate/20130916102548_create_install_script_user.rb
@@ -1,6 +1,6 @@
 class CreateInstallScriptUser < ActiveRecord::Migration
   def change
-  	u = User.create(id: 0, email: 'install_script', password: 'password', role: 'INSTALL_SCRIPT')
+  	u = User.create(id: 1000, email: 'install_script', password: 'password', role: 'INSTALL_SCRIPT')
   	ActiveRecord::Base.connection.execute("update average_entries set user_id = 0 where user_id is null");
   end
 end
